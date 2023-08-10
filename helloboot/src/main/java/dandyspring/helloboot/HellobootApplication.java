7f3778fb2f92ea019ec3dfaa25fbe6559aebf426
postforty <75767073+postforty@users.noreply.github.com>
2023-08-10 09:45:35 +0900

    [ADD] 서블릿 등록

--- helloboot/src/main/java/dandyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/dandyspring/helloboot/HellobootApplication.java
@@ -1,15 +1,37 @@
 package dandyspring.helloboot;
 
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
 public class HellobootApplication {
 
 	public static void main(String[] args) {
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer();
+		WebServer webServer = serverFactory.getWebServer(new ServletContextInitializer() {
+			@Override
+			public void onStartup(ServletContext servletContext) throws ServletException {
+				servletContext.addServlet("hello", new HttpServlet() {
+					@Override
+					protected void service(HttpServletRequest req, HttpServletResponse resp)
+							throws ServletException, IOException {
+						resp.setStatus(200);
+						resp.setHeader("Content-Type", "text/plain");
+						resp.getWriter().println("Hello Servlet");
+					}
+				}).addMapping("/hello");
+			}
+		});
 		webServer.start();
 		
 	}
