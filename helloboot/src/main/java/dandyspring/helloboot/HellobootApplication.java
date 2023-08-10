d2fc77e53bc044c52c8bd8c3f774694a30616851
postforty <75767073+postforty@users.noreply.github.com>
2023-08-10 09:52:45 +0900

    [UPDATE] 서블릿 요청 처리

--- helloboot/src/main/java/dandyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/dandyspring/helloboot/HellobootApplication.java
@@ -13,6 +13,9 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 public class HellobootApplication {
 
@@ -25,9 +28,12 @@ public class HellobootApplication {
 					@Override
 					protected void service(HttpServletRequest req, HttpServletResponse resp)
 							throws ServletException, IOException {
-						resp.setStatus(200);
-						resp.setHeader("Content-Type", "text/plain");
-						resp.getWriter().println("Hello Servlet");
+						String name = req.getParameter("name");
+						
+						resp.setStatus(HttpStatus.OK.value());
+//						resp.setHeader("Content-Type", "text/plain");
+						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println("Hello " + name);
 					}
 				}).addMapping("/hello");
 			}
