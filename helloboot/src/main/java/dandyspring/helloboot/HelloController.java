3567c71ff076e6454a0b443f3b7bb3371b73d5fe
postforty <75767073+postforty@users.noreply.github.com>
2023-08-13 23:38:13 +0900

    Update HelloController.java

--- helloboot/src/main/java/dandyspring/helloboot/HelloController.java
+++ helloboot/src/main/java/dandyspring/helloboot/HelloController.java
@@ -10,3 +10,5 @@ public class HelloController {
 		return helloService.sayHello(Objects.requireNonNull(name));
 	}
 }
+
+// https://www.inflearn.com/course/lecture?courseSlug=%ED%86%A0%EB%B9%84-%EC%8A%A4%ED%94%84%EB%A7%81%EB%B6%80%ED%8A%B8-%EC%9D%B4%ED%95%B4%EC%99%80%EC%9B%90%EB%A6%AC&unitId=138254&tab=curriculum
